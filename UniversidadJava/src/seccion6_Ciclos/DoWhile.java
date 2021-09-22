f1cdbca47fea1c7ff8a4bebf7567cae872e27504
luisdavidsj <luisdavidspider@gmail.com>
2021-09-22 17:20:12 -0500

    Ciclos y Clases

---
+++ UniversidadJava/src/seccion6_Ciclos/DoWhile.java
@@ -0,0 +1,13 @@
+package seccion6_Ciclos;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		var contador = 0;
+		do{
+			System.out.println("contador = " + contador);
+			contador++;
+		}while( contador < 3);
+	}
+
+}
---
+++ UniversidadJava/src/seccion6_Ciclos/For.java
@@ -0,0 +1,11 @@
+package seccion6_Ciclos;
+
+public class For {
+
+	public static void main(String[] args) {
+		for( var contador = 0 ; contador < 3 ; contador++ ){
+            System.out.println("contador = " + contador);
+        }
+	}
+
+}
