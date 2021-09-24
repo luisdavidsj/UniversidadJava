6fdfc5b81d36bad69d84b397a094a0f0ad1a7464
luisdavidsj <luisdavidspider@gmail.com>
2021-09-24 15:58:05 -0500

    Sección 12: Return y Null

--- UniversidadJava/src/seccion11_PasoPorReferencia/PasoPorReferencia.java
+++ UniversidadJava/src/seccion11_PasoPorReferencia/PasoPorReferencia.java
@@ -7,11 +7,12 @@ public class PasoPorReferencia {
 		Persona persona1 = new Persona();
 		persona1.nombre = "Juan";
 		System.out.println("persona1 nombre: " + persona1.nombre);
-		cambiarValor(persona1);
+		persona1 = cambiarValor(persona1);
 		System.out.println("persona1 cambio nombre: " + persona1.nombre);
 	}
 
-	public static void cambiarValor(Persona persona) {
+	public static Persona cambiarValor(Persona persona) {
 		persona.nombre = "Karla";
+		return persona;
 	}
 }
