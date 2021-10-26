216f6e0854640ff8059b711753635c346be8ac7c
Luis David Saenz <luisdavidspider@gmail.com>
2021-10-26 14:29:03 -0500

    Seccion 20: Arreglos en Java

---
+++ UniversidadJava/src/s20_test/TestArreglos.java
@@ -0,0 +1,30 @@
+package s20_test;
+
+public class TestArreglos {
+
+	public static void main(String[] args) {
+		//int []edades =  new int[3];
+		int edades[] = new int[3];
+		System.out.println("edades = " + edades);
+		
+		edades[0] = 10;
+		System.out.println("edades 0 = " + edades[0]);
+		
+		edades[1] = 5;
+		System.out.println("edades 1 = " + edades[1]);
+		
+		edades[2] = 3;
+		System.out.println("edades 2 = " + edades[2]);
+		
+		//edades[3] = 5;
+		
+		for (int i = 0; i < edades.length; i++) {
+			System.out.println("edades elemento " + i + ": " + edades[i]);
+		}
+		
+		String frutas[] = {"Naranja","Platano","Uva"};
+		for (int i = 0; i < frutas.length; i++) {
+			System.out.println("frutas = " + frutas[i]);
+		}
+	}
+}
